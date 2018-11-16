c0e6e1dd67cc597c910662afde3b6c129608462d
19cam92 <cameronfindlay123456789@gmail.com>
2018-11-16 17:23:14 +1100

    First Release Version 1.0.0

---
+++ ARM64/Build-API27-Stock-ARM64.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SAUCE=~/android/opengapps
+ARCH=arm64
+API=27
+
+###############################################################################################################################################
+#Num  Colour    #define         R G B
+
+#0    black     COLOR_BLACK     0,0,0
+#1    red       COLOR_RED       1,0,0
+#2    green     COLOR_GREEN     0,1,0
+#3    yellow    COLOR_YELLOW    1,1,0
+#4    blue      COLOR_BLUE      0,0,1
+#5    magenta   COLOR_MAGENTA   1,0,1
+#6    cyan      COLOR_CYAN      0,1,1
+#7    white     COLOR_WHITE     1,1,1
+
+yellow=`tput setaf 3`
+green=`tput setaf 2`
+cyan=`tput setaf 6`
+red=`tput setaf 1`
+reset=`tput sgr0`
+
+printf '\033]2;%s\007' "Building $ARCH SDK-$API Stock GAPPS"
+echo "${cyan}Building $ARCH SDK-$API Stock GAPPS"
+echo ""
+echo "Writing by 19cam92@xda"
+echo "Script version 1.0.0${reset}"
+
+echo " "
+cd $SAUCE
+make $ARCH-$API-stock
+echo " "
+echo "${green}Done!${reset}"
+read
---
+++ Update_Sources.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SAUCE=~/android/opengapps
+
+###############################################################################################################################################
+#Num  Colour    #define         R G B
+
+#0    black     COLOR_BLACK     0,0,0
+#1    red       COLOR_RED       1,0,0
+#2    green     COLOR_GREEN     0,1,0
+#3    yellow    COLOR_YELLOW    1,1,0
+#4    blue      COLOR_BLUE      0,0,1
+#5    magenta   COLOR_MAGENTA   1,0,1
+#6    cyan      COLOR_CYAN      0,1,1
+#7    white     COLOR_WHITE     1,1,1
+
+yellow=`tput setaf 3`
+green=`tput setaf 2`
+cyan=`tput setaf 6`
+red=`tput setaf 1`
+reset=`tput sgr0`
+
+printf '\033]2;%s\007' "Updating GAPPS Sources"
+echo "${cyan}Updating GAPPS Sources"
+echo ""
+echo "Writing by 19cam92@xda"
+echo "Script version 1.0.0${reset}"
+
+echo " "
+cd $SAUCE
+./download_sources.sh --shallow
+echo " "
+echo "${green}Done!${reset}"
+read
