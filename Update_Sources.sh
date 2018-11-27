36ceeb4bb9d45cea233dbbe60612d2c4445fd051
19cam92 <cameronfindlay123456789@gmail.com>
2018-11-28 10:50:48 +1100

    Added option to install needed build tools

--- Update_Sources.sh
+++ Update_Sources.sh
@@ -27,6 +27,35 @@ echo "Writing by 19cam92@xda"
 echo "Script version 1.0.0${reset}"
 
 echo " "
+echo "${red}!!!! THIS SCRIPT NEEDS ROOT TO INSTALL BUILD TOOLS !!!!${reset}"
+echo "${red}!!!!    PLEASE ENTER YOUR PASSWORD WHEN PROMTED    !!!!${reset}"
+echo " "
+sudo whoami
+
+# Install Android build tools for Ubuntu
+echo " "
+echo -n "${yellow}Install Android build tools for Ubuntu (y/n)? ${reset}"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    sudo apt-add-repository ppa:maarten-fonville/android-build-tools 
+    sudo apt-get update
+    sudo apt-get install android-build-tools-installer
+else
+    echo "Skipping Android build tools for Ubuntu"
+fi
+
+# Install lzip
+echo " "
+echo -n "${yellow}Install lzip (y/n)? ${reset}"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    sudo apt-get install lzip
+else
+    echo "Skipping lzip"
+fi
+
+# Updates Sources
+echo " "
 cd $SAUCE
 ./download_sources.sh --shallow
 echo " "
